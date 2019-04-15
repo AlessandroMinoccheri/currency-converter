48c775e2dcdf8a923effe302faded9fb4af576db
Alessandro Minoccheri <alessandro.minoccheri@gmail.com>
2019-04-15 09:22:29 +0200

    improved rates covergae (#9)

--- tests/RatesTest.php
+++ tests/RatesTest.php
@@ -27,4 +27,34 @@ class RatesTest extends \PHPUnit\Framework\TestCase
 
         $this->assertEquals(0, $result);
     }
+
+    public function testReturnRates()
+    {
+        $from = 'FOO';
+        $to = 'BAR';
+        $expected = 100;
+
+        $this->apiCaller = $this
+            ->getMockBuilder('CurrencyConverter\ApiCaller')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->rates = new Rates($this->apiCaller);
+
+        $this->apiCaller->expects($this->once())
+            ->method('convert')
+            ->with('FOO', 'BAR');
+
+        $this->apiCaller->expects($this->once())
+            ->method('isLastCallEmpty')
+            ->willReturn(false);
+
+        $this->apiCaller->expects($this->once())
+             ->method('getLastResponse')
+             ->willReturn(json_encode([$from . '_' . $to => $expected]));
+
+        $result = $this->rates->getRates($from, $to);
+
+        $this->assertEquals($expected, $result);
+    }
 }
